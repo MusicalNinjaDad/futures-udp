85b901d3d0f31ce067664181719b5773d62dfec8
Mike Foster <i.am.mike.foster@gmail.com>
2026-07-10 09:47:41 +0000

    patch & use bool_to_result probe

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use build_safely::prelude::*;
 fn main() -> Result<()> {
     let ac = AutoCfg::new()?;
     let allowed_features = cargo_allowed_features()?;
-    ac.emit_unstable_feature(OtherFeature("bool_to_result".to_string()), &allowed_features);
+    ac.emit_unstable_feature(bool_to_result, &allowed_features);
     ac.emit_unstable_feature(never_type, &allowed_features);
     Ok(())
 }
